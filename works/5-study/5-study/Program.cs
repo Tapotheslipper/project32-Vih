022ea5d382c0716161ae6efd37f310d9d2474e91
tapo <hypergag@gmail.com>
2025-10-04 09:27:31 +0400

    5-study

---
+++ works/5-study/5-study/Program.cs
@@ -0,0 +1,20 @@
+﻿interface IWorker
+{
+    void Work();
+}
+
+abstract class Employee : IWorker
+{
+    public string name { get; set; }
+    public Employee(string name) => this.name = name;
+    public abstract void Work();
+}
+
+class Developer : Employee
+{
+    public Developer(string name) : base(name) { }
+    public override void Work()
+    {
+        Console.WriteLine($"{this.name} пишет код и сосёт нахуй.");
+    }
+}
