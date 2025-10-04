db31b97d252b136837e3ddeb5f3d1f26544d0001
tapo <hypergag@gmail.com>
2025-10-04 17:33:44 +0400

    аспи
    так называемый

---
+++ works/7-asp/Controllers/HomeyController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeyController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+    public IActionResult About() {
+        return View();
+    }
+}
